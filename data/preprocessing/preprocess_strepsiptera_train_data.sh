82482c2bfe2b93b850baa88f4417ff475b5a5f99
AlinaLeu <45297114+AlinaLeu@users.noreply.github.com>
2020-05-19 13:24:57 +0200

    update strepsiptera train-set

--- data/preprocessing/preprocess_strepsiptera_train_data.sh
+++ data/preprocessing/preprocess_strepsiptera_train_data.sh
@@ -4,4 +4,4 @@
 python3 data_simulator_strepsiptera.py "../raw/strepsiptera/quartet-tree-parameters/" "../raw/strepsiptera/model-params-msa/param-table.tsv" 20000 '../interim/strepsiptera/sim_freq_train.csv'
 
 # shuffle data
-python3 shuffle_data.py -i '../interim/strepsiptera/sim_freq_train.csv' -o '../processed/strepsiptera/train/sim_freq_train_shuffled.csv
+python3 shuffle_data.py -i '../interim/strepsiptera/sim_freq_train.csv' -o '../processed/strepsiptera/train/sim_freq_train_shuffled.csv'
